952dab95d5b617a6832deda760e31df0ff89eecd
Jonathan King <jonathan.j.king@gmail.com>
2016-03-08 01:48:33 +0000

    works if slow

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const colors = require('colors');
 const pty = require('pty.js');
 const args = process.argv.slice(2);
 
-console.log('begin')
+const HEIGHT = process.stdout.rows;
+const WIDTH = process.stdout.columns;
+const LEFTWALL = 10;
+const RIGHTWALL = 10;
+const MIDWIDTH = ~~(WIDTH / 2);
+const MIDHEIGHT = ~~(HEIGHT / 2);
+const FPS = 5;
+const FRAMERATE = ~~(1000 / FPS);
+const DIFFICULTY = 0.5; // Lower is harder
+const STARTTIME = new Date().getTime();
+const PLAYERLINE = ~~(HEIGHT / 4 + HEIGHT / 2);
 
 var npm = pty.spawn('npm', args, {
   cwd: process.cwd()
@@ -30,27 +40,39 @@ npm.stdout.on('end', d => {
 });
 
 
-const HEIGHT = process.stdout.rows;
-const WIDTH = process.stdout.columns;
-const LEFTWALL = 10;
-const RIGHTWALL = 10;
-const MIDWIDTH = ~~(WIDTH / 2);
-const MIDHEIGHT = ~~(HEIGHT / 2);
-const FPS = 30;
-const FRAMERATE = ~~(1000 / FPS);
-const DIFFICULTY = 0.5; // Lower is harder
-const STARTTIME = new Date().getTime();
-
 let SCORE = 0;
 
 term.clear();
 
 const gameState = [{
   type: 'player',
-  s: '^'.blue,
-  up: ~~(HEIGHT / 4 + HEIGHT / 2),
-  left: MIDWIDTH
+  s: '^',
+  up: PLAYERLINE,
+  left: MIDWIDTH,
+  colour: 'blue'
 }];
+
+function Monster (text) {
+  function getPlacement () {
+    let randomStart = ~~(Math.random() * (WIDTH - RIGHTWALL - LEFTWALL - text.length));
+    randomStart = Math.min(randomStart, WIDTH - RIGHTWALL - text.length);
+    randomStart = Math.max(randomStart, LEFTWALL);
+    console.log(randomStart)
+    return randomStart;
+  }
+
+  Object.assign(this, {
+    type: 'mob',
+    s: text,
+    up: 0,
+    left: getPlacement(),
+    colour: 'red'
+  });
+
+  setInterval(() => {
+    this.up++;
+  }, FRAMERATE);
+}
 
 function isInstall (args) {
   return args[0] === 'i' || args[0] === 'install';
@@ -66,25 +88,37 @@ function paintScreen () {
       paintMovers(line);
     }
 
-    term.nl().clearLine();
+    term.nl();
   }
 }
 
 function paintMovers (line) {
-  const arr = gameState.filter(item => item.up === line);
+  const arr = gameState.filter(item => item.up === line).sort((a, b) => a.left > b.left);
+
+  function cursorReturn (item) {
+    term.left(item.left + item.s.length)
+  }
 
   if (arr.length > 0) {
+    // console.log(arr)
     arr.forEach(item => {
-      term.right(item.left).write(item.s).left(item.left);
+      term.right(item.left).write(item.s[item.colour]);
+      cursorReturn(item)
     })
-    const dangerZone = [];
-    arr.forEach(item => {
-      for (var i = item.left; i < item.left + item.s.length; i++) {
-        dangerZone.push(i);
+    if (line === PLAYERLINE) {
+      const playerPos = arr.find(item => item.type === 'player').left;
+      const dangerZone = [];
+      arr.filter(item => item.type === 'mob')
+        .forEach(item => {
+          for (var i = item.left; i < item.left + item.s.length; i++) {
+            dangerZone.push(i);
+          }
+        });
+      if (intersects(playerPos, dangerZone)) {
+        term.nl();
+        console.log(playerPos, dangerZone, arr)
+        gameOver()
       }
-    })
-    if (playerHitMobIn(dangerZone)) {
-      gameOver();
     }
   }
 }
@@ -93,19 +127,17 @@ function gameOver (win) {
   if (win) {
     writeCentre(`You win! Packages installed! Your score was ${SCORE}.`);
   } else {
-    writeCentre(`You lose! Go home and sell your keyboard on ebay. You suck.\nNo packages have been installed but then I'm sure you've got used to failure by now.\n\nYour score was ${SCORE}.`);
+    writeCentre(`You lose! Give up, go home, and sell your keyboard on ebay.\nNo packages have been installed but then I'm sure you've got used to failure by now.\n\nYour score was ${SCORE}.`);
   }
   process.exit();
 }
 
 function writeCentre (msg, offset) {
-  term.nl(MIDHEIGHT).clear().up(MIDHEIGHT).right(MIDWIDTH - msg.length).write(msg).nl(MIDHEIGHT);
+  term.nl(MIDHEIGHT).write(msg).nl(MIDHEIGHT);
 }
 
-function playerHitMobIn (arr) {
-  const player = arr.find(item => item.type === 'player');
-  const enemyPositions = arr.filter(i => i.type === 'mob').map(i => i.left);
-  if (player && enemyPositions.indexOf(player.left) !== -1 ) {
+function intersects (playerPos, dangerZone) {
+  if (dangerZone.indexOf(playerPos) !== -1 ) {
     return true;
   }
   return false;
@@ -123,23 +155,10 @@ function setScore () {
   SCORE = new Date().getTime() - STARTTIME;
 }
 
-function Monster (text) {
-  Object.assign(this, {
-    type: 'mob',
-    s: text.red,
-    up: 0,
-    left: Math.max(Math.min(~~(Math.random() * WIDTH - text.length), WIDTH - RIGHTWALL), LEFTWALL)
-  });
-
-  setInterval(() => {
-    this.up++;
-  }, FRAMERATE);
-}
-
-setInterval(function(){
+function runLoop () {
   generateScene();
   paintScreen();
-}, FRAMERATE);
+};
 
 keypress(process.stdin);
 process.stdin.on('keypress', function (chunk, key) {
@@ -157,3 +176,5 @@ process.stdin.on('keypress', function (chunk, key) {
 });
 process.stdin.setRawMode(true); 
 process.stdin.resume();
+
+setInterval(runLoop, FRAMERATE);
