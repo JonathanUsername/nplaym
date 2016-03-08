cbefef966390806cee930d8f3e2548558df4cf5e
Jonathan King <jonathan.j.king@gmail.com>
2016-03-08 01:03:55 +0000

    sort of working, install curses

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+#!/usr/local/bin/node
+
 'use strict'
 
 const term = require('node-terminal');
 const keypress = require('keypress');
 const colors = require('colors');
+const pty = require('pty.js');
+const args = process.argv.slice(2);
+
+console.log('begin')
+
+var npm = pty.spawn('npm', args, {
+  cwd: process.cwd()
+})
+
+const installingPackages = {};
+
+npm.stdout.on('data', d => {
+  // New npm
+  const bits = d.toString().match(/:[a-zA-Z0-9\-_]+/)
+  if (bits) {
+    const packName = bits[0].slice(1);
+    installingPackages[packName] = 1;
+  }
+});
+
+npm.stdout.on('end', d => {
+  gameOver(true);
+});
+
 
 const HEIGHT = process.stdout.rows;
 const WIDTH = process.stdout.columns;
+const LEFTWALL = 10;
+const RIGHTWALL = 10;
 const MIDWIDTH = ~~(WIDTH / 2);
 const MIDHEIGHT = ~~(HEIGHT / 2);
-const FPS = 60;
+const FPS = 30;
 const FRAMERATE = ~~(1000 / FPS);
 const DIFFICULTY = 0.5; // Lower is harder
 const STARTTIME = new Date().getTime();
@@ -24,35 +52,57 @@ const gameState = [{
   left: MIDWIDTH
 }];
 
+function isInstall (args) {
+  return args[0] === 'i' || args[0] === 'install';
+}
+
 function paintScreen () {
-  for (let i = 0; i < HEIGHT; i++) {
+  for (let line = 0; line < HEIGHT; line++) {
 
-    const arr = gameState
-      .filter(item => item.up === i);
-
-    if (arr.length > 0) {
-      arr.forEach(item => {
-        term.right(item.left).write(item.s).left(item.left);
-        item.tick ? item.tick() : false;
-      })
-      if (intersect(arr)) {
-        writeCentre(`BOOM. Game over. Your score was ${SCORE}`);
-        process.exit();
-      }
-    }
-    if (i === 4) {
+    if (line === 4) {
       setScore();
-      term.left(0).write(SCORE);
+      term.clearLine().left(0).write(SCORE);
+    } else {
+      paintMovers(line);
     }
-    term.write('').nl();
+
+    term.nl().clearLine();
   }
+}
+
+function paintMovers (line) {
+  const arr = gameState.filter(item => item.up === line);
+
+  if (arr.length > 0) {
+    arr.forEach(item => {
+      term.right(item.left).write(item.s).left(item.left);
+    })
+    const dangerZone = [];
+    arr.forEach(item => {
+      for (var i = item.left; i < item.left + item.s.length; i++) {
+        dangerZone.push(i);
+      }
+    })
+    if (playerHitMobIn(dangerZone)) {
+      gameOver();
+    }
+  }
+}
+
+function gameOver (win) {
+  if (win) {
+    writeCentre(`You win! Packages installed! Your score was ${SCORE}.`);
+  } else {
+    writeCentre(`You lose! Go home and sell your keyboard on ebay. You suck.\nNo packages have been installed but then I'm sure you've got used to failure by now.\n\nYour score was ${SCORE}.`);
+  }
+  process.exit();
 }
 
 function writeCentre (msg, offset) {
   term.nl(MIDHEIGHT).clear().up(MIDHEIGHT).right(MIDWIDTH - msg.length).write(msg).nl(MIDHEIGHT);
 }
 
-function intersect (arr) {
+function playerHitMobIn (arr) {
   const player = arr.find(item => item.type === 'player');
   const enemyPositions = arr.filter(i => i.type === 'mob').map(i => i.left);
   if (player && enemyPositions.indexOf(player.left) !== -1 ) {
@@ -62,8 +112,10 @@ function intersect (arr) {
 }
 
 function generateScene () {
-  if (Math.random() > DIFFICULTY) {
-    gameState.push(new Monster);
+  const packages = Object.keys(installingPackages);
+  if (packages.length > 0) {
+    const lastMsg = packages.pop();
+    gameState.push(new Monster(lastMsg));
   }
 }
 
@@ -71,12 +123,12 @@ function setScore () {
   SCORE = new Date().getTime() - STARTTIME;
 }
 
-function Monster () {
+function Monster (text) {
   Object.assign(this, {
     type: 'mob',
-    s: 'X'.red,
+    s: text.red,
     up: 0,
-    left: ~~(Math.random() * WIDTH)
+    left: Math.max(Math.min(~~(Math.random() * WIDTH - text.length), WIDTH - RIGHTWALL), LEFTWALL)
   });
 
   setInterval(() => {
@@ -93,12 +145,13 @@ keypress(process.stdin);
 process.stdin.on('keypress', function (chunk, key) {
   if (!key) return; 
   if (key.ctrl && key.name == 'c') process.exit();
+  const player = gameState.find(i => i.type === 'player');
   switch (key.name) {
     case 'left':
-      gameState.find(i => i.type === 'player').left--;
+      player.left = Math.max(player.left - 1, LEFTWALL);
     break;
     case 'right':
-      gameState.find(i => i.type === 'player').left++;
+      player.left = Math.min(player.left + 1, WIDTH - RIGHTWALL);
     break;
   }
 });
