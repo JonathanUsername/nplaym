480ecf1e02154646dff45bfcad18081fa2cd01a9
Jonathan King <jonathan.j.king@gmail.com>
2016-03-12 11:51:31 +0000

    Kill child processes on exit correctly

--- src/io.js
+++ src/io.js
@@ -42,24 +42,20 @@ function startListening () {
     gameOver(true);
   });
 
+  process.on('exit', function () {
+    npm.kill('SIGINT');
+  });
+
   process.stdin.setRawMode(true);
 
-  // process.stdin.on('data', (chunk) => {
-  //   if (chunk !== null) {
-  //     const str = chunk.toString('utf-8');
-  //     console.log(str)
-  //     if (str === ' ' || str === '\x1b ') return fire(); // Space
-  //     const parts = /^(?:\x1b+)(O|N|\[|\[\[)(?:(\d+)(?:;(\d+))?([~^$])|(?:1;)?(\d+)?([a-zA-Z]))/.exec(str);
-  //     const code = (parts[1] || '') + (parts[2] || '') + (parts[4] || '') + (parts[6] || '')
-  //     if (code === 'OD') return player.left = Math.max(player.left - 1, LEFTWALL); // Left
-  //     if (code === 'OC') return player.left = Math.min(player.left + 1, WIDTH - RIGHTWALL); // Right
-  //   }
-  // })
-
   keypress(process.stdin);
+
   process.stdin.on('keypress', function (chunk, key) {
     if (!key) return; 
-    if (key.ctrl && key.name == 'c') process.exit(1);
+    if (key.ctrl && key.name == 'c') {
+      npm.kill('SIGINT');
+      process.exit(1);
+    }
     switch (key.name) {
       case 'left':
         player.left = Math.max(player.left - 1, LEFTWALL);
