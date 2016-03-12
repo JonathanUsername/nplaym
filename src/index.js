fad6881d6e7636bc77a24852f093e16ec31685e3
Jonathan King <jonathan.j.king@gmail.com>
2016-03-12 11:51:12 +0000

    version checking for npm

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 
 import { startGame } from './game.js';
 import { startListening } from './io.js';
+import { execSync } from 'child_process';
+
+const npmVersion = parseInt(execSync('npm --version').toString());
+if (npmVersion < 3) {
+  console.log('This game only works properly with NPM 3, please consider upgrading!')
+}
 
 startListening();
 startGame();
